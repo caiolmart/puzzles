a610f849fa95cddd987b743f2ed1325b6f593818
Caio Lorenzetti Martinelli <caio.martinelli@hotmail.com>
2021-07-05 22:27:15 -0300

    Optimize - remove cur_direction - 3.63s

--- puzzle7/problem_a.py
+++ puzzle7/problem_a.py
@@ -2,10 +2,9 @@ from typing import List, Tuple
 
 
 class State:
-    def __init__(self, visited, cur_chair, cur_direction):
+    def __init__(self, visited, cur_chair):
         self.visited = visited
         self.cur_chair = cur_chair
-        self.cur_direction = cur_direction
 
 
 class KombuchaProblem:
@@ -25,14 +24,13 @@ class KombuchaProblem:
         and the current direction (-1 for left 1 to right - starts at 0). """
 
         visited = [0 for _ in range(self.n_chairs)]
-        return State(visited, 0, 0)
+        return State(visited, 0)
 
     def actions(self, state: State) -> List[Tuple[int, float]]:
         """ Returns tuples with actions and their probabilities."""
-        if state.visited[state.cur_chair] == 0:
-            left_p, right_p = self.left_right_probabilities(state.cur_chair)
-            return [(-1, left_p), (1, right_p)]
-        return [(state.cur_direction, 1)]
+        
+        left_p, right_p = self.left_right_probabilities(state.cur_chair)
+        return [(-1, left_p), (1, right_p)]
 
     def next_state(self, state: State, action: int):
         """ Returns new state with action. """
@@ -43,16 +41,18 @@ class KombuchaProblem:
         def _pos_left(state, n_chairs):
             return 0 if state.cur_chair == n_chairs - 1 else state.cur_chair + 1
         new_state = State(state.visited.copy(),
-                          state.cur_chair,
-                          state.cur_direction)
+                          state.cur_chair)
         new_state.visited[new_state.cur_chair] = 1
+        cost = 0
         if action == 1:
-            new_state.cur_chair = _pos_right(new_state, self.n_chairs)
-            new_state.cur_direction = 1
+            while new_state.visited[new_state.cur_chair]:
+                new_state.cur_chair = _pos_right(new_state, self.n_chairs)
+                cost += 1
         else:
-            new_state.cur_chair = _pos_left(new_state, self.n_chairs)
-            new_state.cur_direction = -1
-        return new_state
+            while new_state.visited[new_state.cur_chair]:
+                new_state.cur_chair = _pos_left(new_state, self.n_chairs)
+                cost += 1
+        return new_state, cost
 
     def is_final_state(self, state):
         """ Checks if state is final. """
@@ -63,7 +63,7 @@ class KombuchaProblem:
 
 
 def get_expected_time(problem: KombuchaProblem, state: State, cache: dict):
-    key = (tuple(state.visited), state.cur_chair, state.cur_direction)
+    key = (tuple(state.visited), state.cur_chair)
     if key in cache:
         return cache[key]
     if problem.is_final_state(state):
@@ -71,10 +71,10 @@ def get_expected_time(problem: KombuchaProblem, state: State, cache: dict):
         return cache[key]
 
     actions = problem.actions(state)
-    total_expected = 1
+    total_expected = 0
     for action in actions:
-        total_expected += action[1] * get_expected_time(
-            problem, problem.next_state(state, action[0]), cache)
+        next_state, cost = problem.next_state(state, action[0])
+        total_expected += action[1] * (cost + get_expected_time(problem, next_state, cache))
     return total_expected
 
 
